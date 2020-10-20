9bc080216c4837544c01769e47e2943816357ff5
Givson21 <72840356+Givson21@users.noreply.github.com>
2020-10-20 12:42:48 +1000

    Using Functions
    Conversion function
    Aggregate func
    Null related func

--- Session 2 continue.sql
+++ Session 2 continue.sql
@@ -34,4 +34,68 @@ EXTRACT(SECOND FROM SYSTIMESTAMP) || ', ' ||
 EXTRACT(DAY FROM SYSTIMESTAMP) ||  '/' ||
 EXTRACT(MONTH FROM SYSTIMESTAMP) ||  '/' ||
 EXTRACT(YEAR FROM SYSTIMESTAMP)  "My Timestamp"
-FROM Dual;
+FROM dual;
+
+/*Using Conversion Functions*/
+
+/*Converting Dates to Characters using format Templates (Format Models)*/
+
+SELECT last_name, hire_date,
+TO_CHAR(hire_date,'FMMonth DD YYYY') "date Started"
+FROM employees;
+
+
+/*Converting Characters to Numbers*/
+
+SELECT city, postal_code "Original Postal Code",
+TO_NUMBER(postal_code) + 1 "Dummy Postal Code"
+FROM locations
+WHERE country_id = 'US';
+
+
+/*Using Aggregate Functions*/
+
+/*Counting the number of employees reporting to the manager (GROUP BY Clause)*/
+
+SELECT manager_id "Manager" ,
+COUNT(*) "Number of Employees"
+FROM employees
+GROUP BY manager_id
+
+/*Filtering Aggregate result using HAVING clause  (Having Clause)*/
+
+SELECT manager_id "Manager" ,
+COUNT(*) "Number of Employees"
+FROM employees
+GROUP BY manager_id
+HAVING manager_id = 214;
+
+/*Using Aggregate Functions for statistical information*/
+
+SELECT job_id,
+MIN(salary) "Minimum",
+MAX(salary) "Maxicum",
+AVG(salary) "Average" ,
+MEDIAN(salary) "Median",
+ROUND(STDDEV(salary),2) "Standard deviation"
+FROM employees
+GROUP BY job_id;
+
+/*Using Null - Related Functions*/
+
+/*Substituting String  Value for Null Values (Reference)*/
+ 
+SELECT last_name, NVL (TO_CHAR(commission_pct), 'Not Applicable')
+FROM employees WHERE last_name LIKE 'B%';
+
+
+/*Specifying different expressions for NULL and NOT NULL Values*/
+
+SELECT last_name, NVL2(commission_pct ,    salary + salary*commission_pct,   salary )  "Income"
+FROM employees
+WHERE last_name LIKE 'B%';
+
+
+
+
+
